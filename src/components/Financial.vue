4409c0f7ce24a6f95153f888475dcbf28118fcdb
amandamichel <amandamichel@users.noreply.github.com>
2020-09-26 16:03:46 -0400

    added page for financial

---
+++ src/components/Financial.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-list shaped>
+      <v-subheader>REPORTS</v-subheader>
+      <v-list-item-group v-model="item" color="primary">
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+        >
+          <v-list-item-icon>
+            <v-icon v-text="item.icon"></v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.text"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      item: 1,
+      items: [
+        { text: 'Capital One', icon: 'mdi-alpha-c-box' },
+        { text: 'Bank of America', icon: 'mdi-alpha-b-box' },
+      ],
+    }),
+  }
+</script>
