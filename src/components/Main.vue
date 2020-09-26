40c12208002beeb7c9e84efee33335ec54d98c02
Dante12129 <dan9coasterdan@gmail.com>
2020-09-26 12:27:17 -0400

    Create List Of Categories

---
+++ src/components/Main.vue
@@ -0,0 +1,27 @@
+<template>
+<v-list>
+  <v-list-item-group v-model="selected">
+    <v-list-item v-for="(cat, index) in categories" :key="index">
+      <v-list-item-content>
+        <v-list-item-title v-text="cat"></v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+  </v-list-item-group>
+</v-list>
+</template>
+
+<script>
+export default {
+  name: "Main",
+  data: function () {
+    return {
+      selected: 1,
+      categories: ['Financial', 'Social', 'Entertainment', 'Shopping']
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
