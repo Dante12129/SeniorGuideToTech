b29adfb6d3e7722c8e6248aec46c3c2e409e79aa
Steven <szjia2010@gmail.com>
2020-09-27 16:34:44 -0400

    social edits

--- src/components/Social.vue
+++ src/components/Social.vue
@@ -1,23 +1,51 @@
 <template>
-  <v-expansion-panels>
+    <v-expansion-panels>
 
-    <v-expansion-panel>
-      <v-expansion-panel-header>Zoom</v-expansion-panel-header>
-      <v-expansion-panel-content>
-        Zoom has become very popular since the coronavirus pandemic. It allows people to see each other through cameras and talk to each other with their microphones, essentially a face-to-face conversation through the internet.
+        <v-expansion-panel>
+            <v-expansion-panel-header>Zoom</v-expansion-panel-header>
+            <v-expansion-panel-content>
+                Zoom has become very popular since the coronavirus pandemic. It allows people to see each other through cameras and talk to each other with their
+                microphones, essentially a face-to-face conversation through the internet.
+                <br><br>
+                The simplest way to join a Zoom call is to click the invitation link. A link will look like this:
+                <br><br>
+                https://cwm.zoom.us/j/55555555555
+                <br><br>
+                If the link is blue and underlined then you can simple click it and it will take you to a page that looks like this (except for the red rectangle):<br>
+                <img src="../images_social/Zoom_Invite.png" width="100%" /> <br> <br>
+                Notice the button called "Open Zoom Meetings" that is boxed by a red rectangle. Click on that button and Zoom should open in a new window: <br>
+                <img src="../images_social/Zoom_Launched.png" width="100%" /><br><br>
+                If this does not show up then you probably got a message telling you to download and install Zoom, this is because Zoom is an application that allows the computer to do new things.
+                Other examples of applications include Word and Microsoft Edge. If your computer is not from Apple, then Word and Microsoft Edge were already installed for you.
+                In this case, we will have to download and install Zoom ourselves. Please refer to the "Installing Zoom" section and return here.
+                <br><br>
+                Click on the blue "Join with Computer Audio" button that is boxed with the red rectangle. At this point you have now joined a Zoom meeting! Notice how the Zoom meeting is in a new "window". The window is box that shows some content inside it. The Zoom window shows "Zoom", that means it is where the meeing video and various buttons that affect Zoom will appear. <br><br>
+                The window:
+                <img src="../images_social/Zoom_Plain.png" width="100%"> <br><br>
+                Move your cursor over the window. Various buttons will appear at the bottom of the window. It will look similar to the following but do not worry if some things are not there.<br>
+                <img src="../images_social/Zoom_Plain_Buttons.png" width="100%"><br><br>
+                The two most important buttons are "Mute" and "Start Video" in the bottom left corner. "Mute"/"Unmute" turns your microphone on and off, the lable will switch between "Mute" and "Unmute" depending on if the microphone is currently on or off.
+                "Start Video"/"Stop Video" turns your camera on and off, the label will change just like "Mute"/"Unmute" depending on if the camera currently is on or off.
+                Use these buttons to control when the other members of the meeting can see and hear you. The other important button is the red one on the right. Clicking it and then clicking the "Leave Meeting" which shows up right after will let you exit the Zoom meeting.
+                <br><br>
 
 
-      </v-expansion-panel-content>
-    </v-expansion-panel>
+            </v-expansion-panel-content>
+        </v-expansion-panel>
+        <v-expansion-panel>
+            <v-expansion-panel-header>Installing Zoom</v-expansion-panel-header>
+            <v-expansion-panel-content>
+                [placeholder]
+            </v-expansion-panel-content>
+        </v-expansion-panel>
+        <v-expansion-panel>
+            <v-expansion-panel-header>Facebook</v-expansion-panel-header>
+            <v-expansion-panel-content>
+                [Instructions go here]
+            </v-expansion-panel-content>
+        </v-expansion-panel>
 
-    <v-expansion-panel>
-      <v-expansion-panel-header>Facebook</v-expansion-panel-header>
-      <v-expansion-panel-content>
-        [Instructions go here]
-      </v-expansion-panel-content>
-    </v-expansion-panel>
-
-  </v-expansion-panels>
+    </v-expansion-panels>
 </template>
 
 <script>
