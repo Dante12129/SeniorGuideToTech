7b2c59a14f86ed85ca8393a215f888638f75126b
amandamichel <amandamichel@users.noreply.github.com>
2020-09-27 00:19:48 -0400

    fleshed out entertainment tab

--- src/components/Entertainment.vue
+++ src/components/Entertainment.vue
@@ -4,14 +4,54 @@
     <v-expansion-panel>
       <v-expansion-panel-header>YouTube</v-expansion-panel-header>
       <v-expansion-panel-content>
-        [Instructions go here]
+        YouTube is a simple video platform that comes at no individual cost. It has a wide variety of genres; 
+        on YouTube you can learn a new skill, watch people cook, listen to music, and much more. Searching for specific 
+        videos and general browsing are both simple and easy. The YouTube homepage is a great place to start! <br/>
+        <img src="../images_entertainment/yt_homepage.png"/> <br/>
+        The videos immediately visible are YouTube's recommendations for you. On the lefthand side are categories to help you browse:
+        specific topics will appear, and clicking on them will allow you to browse popular videos in those categories. The trending tab 
+        will show you the most popular videos currently circulating YouTube. Now, let's take a look at the search function. <br/>
+        <img src="../images_entertainment/yt_search.png"/> <br/>
+        Note that the categories from the homepage are there even when you make a specific search. Here, I performed a search for 
+        "cat videos". The results youtube provided me are the most relevant videos that match my search input. However, there are
+        ways to make our search even more specific! Clicking the filter button near the top will bring us to the following drop-down
+        menu: <br/>
+        <img src="../images_entertainment/yt_filter.png"/> <br/>
+        Through this feature, we can select any of the listed attributes and apply them to our search. For example, if I want to watch
+        a cat video, but I don"t have a lot of time, I would make this search, click "filter", and under duration select "short". <br/>
+        What if something goes wrong? In general, the first thing to do is reload the page. You can also try disconnecting and reconnecting 
+        to your WiFi. If this doesn't work, we have two main options:
+        search "YouTube troubleshooting" in google for general help, or google specifically what is going wrong. Generally, the best 
+        results for this are from "support.google.com" or directly from YouTube.
       </v-expansion-panel-content>
     </v-expansion-panel>
 
     <v-expansion-panel>
       <v-expansion-panel-header>Which Streaming Service is for Me?</v-expansion-panel-header>
       <v-expansion-panel-content>
-        [Instructions go here]
+        The world of video streaming services is very expansive; we have plenty of options. Unfortunately, this means we have a lot of 
+        information to parse through. Here, we will go through the most common streaming services, and what they are known for. To start, 
+        let's look at Netflix.
+        Netflix was the first of its kind. It has a very wide selection of TV and movies with no particular niche. A standard Netflix 
+        subscription comes without advertisements during and between epsiodes or movies. Netflix releases TV shows in full seasons, so it 
+        is not the best choice for watching new shows as they air. The service is also gaining a reputation in production. "Netflix Originals"
+        are like the rest of the platform: wide-ranging. From drama, to documentaries, to reality television, Netflix has a wide range. Overall,
+        Netflix is a great general option, but lacks in the ways of live TV. <br/>
+        Now, we will look at Hulu. The standard subscription of Hulu comes with access to some of their titles, with ads in between and during 
+        movies and tv. Hulu drops TV episodes as they come out; if a show releases new epsiodes on cable every Wednesday, Hulu will drop that
+        episode on Thursday. Hulu can be enhanced with channel extensions and Hulu+Live TV. The former allows you access to titles from a 
+        specific channel, like HBO or Starz. The latter allows you access to more of Hulu's library and access to TV episodes live as they air 
+        on cable. "Ad Free" Hulu is another extension that you can purchase that will stop ads from playing between and during your titles. Hulu
+        is great for new shows, but to gain access to more of what the service has to offer, you will have to pay for extensions. <br/>
+        Next up: Disney+. The advantage of Disney+ is simple: the titles available are the titles Disney owns. This means classic Disney, Marvel,
+        Pixar, National Geographic, and -my personal favorite- Star Wars. These are the only titles on Disney+, but their reach is expansive. In 
+        short, Disney+ is good for Disney content. <br/>
+        Finally, we will look at ESPN+. This service is very similar to Disney+ in that it has a very specific title selection: sports. You can stream 
+        games live and play them back later. <br/>
+        Streaming services all function in basically the same way: you need an internet connection to access the titles. Now, what if something goes 
+        wrong here? The approach is largely similar to YouTube, in that that the first thing to do is reload the page, and potientially disconnecting and
+        reconnecting to your WiFi. If these options do not work, google search "[name of streaming service] troubleshooting" or directly search your specific 
+        problem. Again, your best approach is probably finding help directly from the streaming service's help site.
       </v-expansion-panel-content>
     </v-expansion-panel>
 
